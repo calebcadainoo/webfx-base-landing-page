6444f7bb490ddac46cf622d1739fd2e4f82b3ad4
Caleb Crosy Adaioo <code.crosby@gmail.com>
2021-02-25 15:25:18 +0000

    added the animation to the navigation menu

--- public/main.js
+++ public/main.js
@@ -12,6 +12,14 @@ hamburger.addEventListener('click', function() {
   
   if (mobileNav.classList.contains('open-menu')) {
     hamburgerImage.src = closeIcon;
+    let num = 0;
+    mobileNav.style.opacity = num.toFixed(1);
+    setInterval (function(){
+      if (num <= 0.9) {
+          num += 0.1;
+          mobileNav.style.opacity = num.toFixed(1);
+      }
+    }, 30);
   } else {
     hamburgerImage.src = menuIcon;
   }
